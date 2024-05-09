a5a3bb1b0ce1a49eb48a5d052a22cb58ef8a3f05
gabryel329 <gabryel329@gmail.com>
2024-05-09 13:12:29 -0300

    first commit

--- app/Http/Controllers/TabelaController.php
+++ app/Http/Controllers/TabelaController.php
@@ -4,39 +4,77 @@ namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
 use GuzzleHttp\Client;
+use PhpOffice\PhpSpreadsheet\IOFactory;
 use Symfony\Component\DomCrawler\Crawler;
 
 class TabelaController extends Controller
 {
-    public function tabela_brasileiro()
-    {
-        // Configurando o cliente cURL com verificação de certificado desativada temporariamente
-        $client = new Client([
-            'verify' => false,
-        ]);
+    // public function tabela_brasileiro()
+    // {
+    //     // Configurando o cliente cURL com verificação de certificado desativada temporariamente
+    //     $client = new Client([
+    //         'verify' => false,
+    //     ]);
     
-        // Realizando a solicitação HTTP usando cURL
-        $response = $client->get('https://iffhs.com/posts/2452');
+    //     // Realizando a solicitação HTTP usando cURL
+    //     $response = $client->get('https://iffhs.com/posts/3153');
     
-        // Obtendo o corpo da resposta HTTP
-        $html = (string) $response->getBody();
+    //     // Obtendo o corpo da resposta HTTP
+    //     $html = (string) $response->getBody();
     
-        // Criando uma instância do Crawler para analisar o HTML
-        $crawler = new Crawler($html);
+    //     // Criando uma instância do Crawler para analisar o HTML
+    //     $crawler = new Crawler($html);
     
-        // Inicializando um array para armazenar os dados da tabela HTML
-        $rows = [];
+    //     // Inicializando um array para armazenar os dados da tabela HTML
+    //     $rows = [];
     
-        // Iterando sobre as linhas da tabela e extraindo os dados das células
-        $crawler->filter('table tr')->each(function ($row) use (&$rows) {
-            $rowData = [];
-            $row->filter('td')->each(function ($cell) use (&$rowData) {
-                $rowData[] = $cell->text();
-            });
-            $rows[] = $rowData;
-        });
+    //     // Iterando sobre as linhas da tabela e extraindo os dados das células
+    //     $crawler->filter('table tr')->each(function ($row) use (&$rows) {
+    //         $rowData = [];
+    //         $row->filter('td')->each(function ($cell) use (&$rowData) {
+    //             $rowData[] = $cell->text();
+    //         });
+    //         $rows[] = $rowData;
+    //     });
 
-        // Retornando a view 'chances' com os dados da tabela HTML e da tabela chancesvit
-        return view('welcome', compact('rows'));
+    //     // Retornando a view 'chances' com os dados da tabela HTML e da tabela chancesvit
+    //     return view('welcome', compact('rows'));
+    // }
+
+    public function atualizar()
+    {
+        // Caminho para o arquivo Excel
+        $filePath = public_path('/Sites.xlsx');
+
+        // Carrega o arquivo Excel
+        $spreadsheet = IOFactory::load($filePath);
+
+        // Obtém a primeira planilha do arquivo
+        $sheet = $spreadsheet->getSheet(0);
+
+        // Obtém o número da última linha
+        $lastRow = 12;
+
+        // Array para armazenar os dados
+        $data = [];
+
+        // Percorre as linhas do arquivo Excel
+        for ($row = 1; $row <= $lastRow; $row++) {
+            // Obtém os valores das células
+            $cellAValue = $sheet->getCell('A' . $row)->getValue();
+            $cellBValue = $sheet->getCell('C' . $row)->getValue();
+            $cellCValue = $sheet->getCell('E' . $row)->getValue();
+
+            // Adiciona os valores ao array
+            $data[] = [
+                'nome' => $cellAValue,
+                'pais' => $cellBValue,
+                'ano' => $cellCValue,
+            ];
+        }
+
+        // Retorna a view com os dados
+        return view('welcome')->with('data', $data);
     }
+
 }
--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -3,44 +3,37 @@
     
 <head>
     <!-- Site Title-->
-    <title>Campeonato Brasileiro</title>
-    <link rel="stylesheet" href="css/styles.css">
+    <title>Melhores Times de Todos os Tempos</title>
+    <link rel="icon" href="images/favicon.png" type="image/x-icon">
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-QWTKZyjpPEjISv5WaRU9OFeRpok6YctnYmDr5pNlyT2bRjXh0JMhjY6hW+ALEwIH" crossorigin="anonymous">
 </head>
 
 <body>
+    
     <div class="container">
-        <div style="align-items: center">
-            <article class="heading-component">
-                <div class="heading-component-inner">
-                    <h5 class="heading-component-title">Campeonato Brasileiro</h5>
-                </div>
-            </article>
-            <div class="table-custom-responsive">
-                <table class="table-custom">
-                    <thead>
+        <div style="text-align: center">
+            <h1>Melhores Times de Todos os Tempos</h1>
+            <div class="table-responsive">
+                <table class="table table-hover table-bordered">
+                    <thead class="table-dark">
                         <tr>
                             <th>#</th>
-                            <th colspan="1">GOLEIROS</th>
-                            <th>PAÍS</th>
-                            <th>TIME</th>
-                            <th>V</th>
+                            <th colspan="1">Nome</th>
+                            <th>País</th>
+                            <th>Ano</th>
                         </tr>
                     </thead>
                     <tbody>
-                        @foreach ($rows as $index => $row)
-                        @if ($index != 0)
+                        @foreach ($data as $index => $row)
+                        @if ($index != 0 and $index != 1) <!-- Excluir a primeira linha -->
                         <tr>
-                            <td>{{ $index }}</td>
-                            <!-- Incorporar a imagem ao lado do nome do time na mesma célula -->
-                            <td >
-                                {{ $row[1] }} <!-- Nome do time -->
-                            </td>
-                            <!-- Loop através de outras células excluindo a primeira que contém o nome -->
-                            @foreach ($row as $cell_index => $cell)
-                            @if ($cell_index != 1) <!-- Pular o índice do nome do time para não duplicar -->
-                                <td>{{ $cell }}</td>
-                            @endif
-                            @endforeach
+                            <!-- Exibir apenas as colunas desejadas -->
+                            <td>{{ $index - 1 }}</td>
+                            <!-- Nome do time -->
+                            <td>{{ $row['nome'] }}</td>
+                            <!-- Colunas de interesse -->
+                            <td>{{ $row['pais']  }}</td> <!-- Gols -->
+                            <td>{{ $row['ano']  }}</td> <!-- Gols -->
                         </tr>
                         @endif
                         @endforeach
@@ -50,5 +43,5 @@
         </div>
     </div>
 </body>
-
+<script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-YvpcrYf0tY3lHB60NNkmXc5s9fDVZLESaAA55NDzOxhy9GkcIdslK1eN7N6jIeHz" crossorigin="anonymous"></script>
 </html>
